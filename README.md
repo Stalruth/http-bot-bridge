d87a8b7df1ce99fdc5e8f2d7e13f92b76b7b0796
Stuart Allan Rutherford <stalruth@gmail.com>
2022-10-19 18:49:20 +1100

    Update deps and use env vars

--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 Takes Interaction events from the Gateway and sends them to HTTP based bots.
 
-Good for testing HTTP based bots as there is no way to get specific details on errors in Interaction Responses.
+Good for testing HTTP based bots as there is no way to get specific details on
+errors in Interaction Responses.
 
 ## Usage
 
-Environment variables:
+Required environment variables:
 
 - `BOT_TOKEN`: The bot's token, required for Gateway access.
 - `PORT`: The port on `localhost` to send Interactions to. Defaults to 8080.
-- `URL`: The path on `localhost` to send Interactions to. Defaults to the empty string.
+- `URL`: The path on `localhost` to send Interactions to. Defaults to the
+    empty string.
 
-Command line parameters:
+Optional environment variables:
 
-- `KEYFILE`: Location to read/write the keys to.
+- `PUBLIC_KEY`: The Public Key to be used on the Bot.
+- `PRIVATE_KEY`: The Private Key used for signing Interaction payloads.
+
+If these are not provided they will be generated and printed to standard
+output.
 
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-
-const { Client, Intents } = require('discord.js');
+const { Client } = require('discord.js');
 const fetch = require('node-fetch');
 const nacl = require('tweetnacl');
 
@@ -11,52 +9,35 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT ?? '8080';
 const PATH = process.env.URL ?? '';
 
-const URL_BASE = 'https://discord.com/api/v9';
-
-const KEYFILE = process.argv[2];
+const URL_BASE = 'https://discord.com/api/v10';
 
 const client = new Client({ intents: [] });
 
-function getKeys(fileName) {
-  if (fileName) {
-    try {
-      const file = fs.readFileSync(fileName);
-      const savedKeys = JSON.parse(file);
-      return {
-        publicKey: new Uint8Array(Buffer.from(savedKeys.publicKey, 'hex')),
-        secretKey: new Uint8Array(Buffer.from(savedKeys.secretKey, 'hex')),
-      };
-    } catch {
-      console.log('Cannot read file, generating key...');
-    }
+function getKeys() {
+  if(!process.env.PUBLIC_KEY || !process.env.SECRET_KEY) {
+    return nacl.sign.keyPair();
+  } else {
+    return {
+      publicKey: new Uint8Array(Buffer.from(process.env.PUBLIC_KEY, 'hex')),
+      secretKey: new Uint8Array(Buffer.from(process.env.SECRET_KEY, 'hex')),
+    };
   }
-  const keyPair = nacl.sign.keyPair();
-  if (fileName) {
-    try {
-      const savedKeys = {
-        publicKey: Buffer.from(keyPair.publicKey).toString('hex'),
-        secretKey: Buffer.from(keyPair.secretKey).toString('hex'),
-      };
-      fs.writeFileSync(fileName, JSON.stringify(savedKeys, null, 2));
-    } catch {
-      console.log('Cannot write keyfile.');
-    }
-  }
-  return keyPair;
 }
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} with public key: ${Buffer.from(keys.publicKey).toString('hex')}`);
-});
+const keys = getKeys();
 
-const keys = getKeys(KEYFILE);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}\nSending Interactions to http://localhost:${PORT}${PATH}\nPublic key: ${Buffer.from(keys.publicKey).toString('hex')}\nSecret key: ${Buffer.from(keys.secretKey).toString('hex')}`);
+});
 
 client.ws.on('INTERACTION_CREATE', async(e) => {
   const timestamp = Buffer.from(`${Date.now()}`);
   const message = Buffer.from(JSON.stringify(e));
   const signature = nacl.sign.detached(Buffer.concat([timestamp, message]), keys.secretKey);
+  const URL = `${URL_BASE}/interactions/${e.id}/${e.token}/callback`;
+
   try {
-    const serverResult = await fetch(`http://localhost:${PORT}${PATH}`, {
+    const botResult = await fetch(`http://localhost:${PORT}${PATH}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -66,26 +47,24 @@ client.ws.on('INTERACTION_CREATE', async(e) => {
       body: message.toString(),
     });
 
-    const URL = `${URL_BASE}/interactions/${e.id}/${e.token}/callback`;
+    const response = await botResult.text();
 
-    const response = await serverResult.text();
+    const discordResult = await fetch(URL, {
+      method: 'POST',
+      body: response,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    console.log(discordResult.status, await discordResult.text());
+
+    if(discordResult.status >= 400) {
+      console.log('Interaction Response body: ', response);
+    }
   } catch(e) {
     console.log(e);
     return;
-  }
-
-  const discordResult = await fetch(URL, {
-    method: 'POST',
-    body: response,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  console.log(discordResult.status, await discordResult.text());
-
-  if(discordResult.status >= 400) {
-    console.log('Interaction Response body: ', response);
   }
 });
 
